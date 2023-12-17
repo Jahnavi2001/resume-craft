08a01470f5ba314c3ad0cda27d08d40b779be981
vuyyuru.jahnavi <vuyyuru.jahnavi@gdn-commerce.com>
2023-12-17 19:09:09 +0530

    2nd template done

--- src/templates/Template2.js
+++ src/templates/Template2.js
@@ -16,240 +16,161 @@ const Template2 = () => {
   });
 
   return (
-    <>
+    <div
+      style={{
+        boxShadow: "0 2px 12px 0 rgba(0, 0, 0, 0.24)",
+      }}
+    >
       <div
         id="template-2"
-        className="w-full"
-        style={{ border: "1px solid #4b6982", backgroundColor: "#f7eebb" }}
+        className="flex h-min"
+        style={{ backgroundColor: "#f7eebb" }}
       >
-        <div className="flex">
-          <div
-            className="flex flex-col items-center pt-5 w-4/12 gap-4"
-            style={{ backgroundColor: "#583131", flexDirection: "column" }}
-          >
-            <div>
+        <div className="flex flex-col items-center w-4/12 gap-4 pt-4 bg-[#583131] px-2">
+          <div className="my-6">
+            {personalInfo.profilePic && (
               <img
-                className="rounded align-self-center  "
+                className="rounded-full w-28 h-28 bg-gray-300"
                 src={personalInfo.profilePic}
                 alt="profile-pic"
-                style={{
-                  maxHeight: "180px",
-                  minHeight: "100px",
-                  width: "100px",
-                  background: "grey",
-                  padding: 0,
-                }}
               />
-            </div>
-
-            <div
-              className="font-bold text-center"
-              style={{ fontFamily: "Serif" }}
-            >
-              <div className="text-white text-xl">
-                {personalInfo.firstName || personalInfoDefault.firstName}{" "}
-                {personalInfo.lastName || personalInfoDefault.lastName}
+            )}
+            {!personalInfo.profilePic && (
+              <div className="w-28 h-28 rounded-full bg-[#f7eebb] text-6xl flex justify-center items-center">
+                {personalInfo.firstName.charAt(0) ||
+                  personalInfoDefault.firstName.charAt(0)}
+                {personalInfo.lastName.charAt(0) ||
+                  personalInfoDefault.lastName.charAt(0)}
               </div>
-              <h5 className="text-[#adccc7] text-md">
-                {personalInfo.jobTitle || personalInfoDefault.jobTitle}
-              </h5>
-            </div>
-
-            <div>
-              <div
-                className="px-2 mb-2"
-                style={{ backgroundColor: "white", color: "black" }}
-              >
-                Email:
-              </div>
-
-              <div style={{ color: "#f7f7f7" }}>
-                {personalInfo.email || personalInfoDefault.email}
-              </div>
-
-              <div
-                className=" px-2 mb-2 mt-2"
-                style={{ backgroundColor: "white", color: "black" }}
-              >
-                Contact:
-              </div>
-
-              <div style={{ color: "#f7f7f7" }}>
-                {" "}
-                {personalInfo.phoneNumber || personalInfoDefault.phoneNumber}
-              </div>
-
-              <div
-                className=" px-2 mb-2 mt-2 "
-                style={{ backgroundColor: "white", color: "black" }}
-              >
-                Address:
-              </div>
-              <div style={{ color: "#f7f7f7" }}>
-                {personalInfo.city || personalInfoDefault.city},{" "}
-                {personalInfo.state || personalInfoDefault.state}
-              </div>
-            </div>
+            )}
           </div>
 
-          <div className="flex flex-col px-8 w-8/12">
-            <div>
-              <div className="text-justify mt-4">
-                {summary || summaryDefault}
-              </div>
-              <hr style={{ height: "5px", backgroundColor: "#4b6982" }} />
+          <div className="font-bold text-center">
+            <div className="text-white text-2xl">
+              {personalInfo.firstName || personalInfoDefault.firstName}{" "}
+              {personalInfo.lastName || personalInfoDefault.lastName}
             </div>
-            <div className="" style={{ fontFamily: "Serif" }}>
-              <div className="">
-                <div
-                  className="text-left bg-light mb-4 "
-                  style={{ color: "#4b6982" }}
-                >
-                  {" "}
-                  <h3>
-                    <b>Professional Experience</b>{" "}
-                  </h3>
-                </div>
-                <div className=" text-left " style={{ fontSize: "18px" }}>
-                  {getWorkHistoryDetails().map((item) => (
-                    <div className="text-sm" key={item.id}>
-                      <div>
-                        <div className="text-lg">{item.positionTitle}</div>
-                        <div
-                          style={{
-                            fontFamily: "Source Sans Pro",
-                            fontSyle: "normal",
-                          }}
-                        >
-                          {item.companyName} |{" "}
-                          {getMMMYYYYFromDateString(item.startDate)} -
-                          {getMMMYYYYFromDateString(item.endDate)}
-                        </div>
-                      </div>
-                      <ul
-                        style={{
-                          fontFamily: "Source Sans Pro",
-                          fontSyle: "normal",
-                        }}
-                      >
-                        {item.summary.split(".").map(
-                          (point) =>
-                            point && (
-                              <li
-                                className="flex gap-3 items-center"
-                                key={point}
-                              >
-                                <div className="w-1 h-1 bg-[#DE8534] rounded-full mt-2"></div>
-                                <div>{point}</div>
-                              </li>
-                            )
-                        )}
-                      </ul>
-                    </div>
-                  ))}
-                  {/* <div>
-                    <div className="mt-2">
-                      <h4>Trans Global JDG</h4>
-                    </div>
-                    <div className="mt-2">
-                      <b>Executive Head</b>
-                    </div>
+            <h5 className="text-[#adccc7]">
+              {personalInfo.jobTitle || personalInfoDefault.jobTitle}
+            </h5>
+          </div>
 
-                    <div className="mt-2 mb-3">
-                      <div>Worked in Trans Global JDG from 2015 to 2018.</div>
-                      <div>
-                        {" "}
-                        Human resources generalist with 8 years of experience in
-                        HR, including hiring and terminating, disciplining
-                        employees and helping department managers improve
-                        employee performance. Worked with labor unions to
-                        negotiate compensation packages for workers. Organized
-                        new hire training initiatives as well as ongoing
-                        training to adhere to workplace safety standards. Worked
-                        with OSHA to ensure that all safety regulations are
-                        followed.
-                      </div>
-                    </div>
-                  </div> */}
-                </div>
-                <div className="w-100 mt-4"> </div>
-                <hr style={{ height: "5px", backgroundColor: "#4b6982" }} />
-                <div
-                  className="bg-light text-left"
-                  style={{ color: "#4b6982" }}
-                >
-                  <h3>
-                    <b>Education</b>
-                  </h3>
-                </div>
-                <div className=" text-left">
-                  <div style={{ fontSize: "18px" }}>
-                    {getWorkHistoryDetails().map((item) => (
-                      <div className="text-sm" key={item.id}>
-                        <div>
-                          <div className="text-lg">{item.positionTitle}</div>
-                          <div
-                            style={{
-                              fontFamily: "Source Sans Pro",
-                              fontSyle: "normal",
-                            }}
-                          >
-                            {item.companyName} |{" "}
-                            {getMMMYYYYFromDateString(item.startDate)} -
-                            {getMMMYYYYFromDateString(item.endDate)}
-                          </div>
-                        </div>
-                        <ul
-                          style={{
-                            fontFamily: "Source Sans Pro",
-                            fontSyle: "normal",
-                          }}
-                        >
-                          {item.summary.split(".").map(
-                            (point) =>
-                              point && (
-                                <li
-                                  className="flex gap-3 items-center"
-                                  key={point}
-                                >
-                                  <div className="w-1 h-1 bg-[#DE8534] rounded-full mt-2"></div>
-                                  <div>{point}</div>
-                                </li>
-                              )
-                          )}
-                        </ul>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-                <div className="w-100 mt-4"> </div>
-                <hr style={{ height: "5px", backgroundColor: "#4b6982" }} />
-                <div className="bg-light text-left ">
-                  <h3 style={{ color: "#4b6982" }}>
-                    <b>Key Skills</b>
-                  </h3>
-                </div>
-                <div className=" text-left mb-4" style={{ fontSize: "18px" }}>
-                  <ul
-                    style={{
-                      fontFamily: "Source Sans Pro",
-                      fontSyle: "normal",
-                    }}
-                  >
-                    {getSkillsDetails().map((item) => (
-                      <li className="flex gap-3" key={item.id}>
-                        <div className="w-[6px] h-[6px] bg-[#DE8534] rounded-full mt-2"></div>
-                        <div>{item.name}</div>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
+          <div className="flex flex-col gap-2">
+            <div className="px-2 bg-white text-black">Email:</div>
+
+            <div className="text-white">
+              {personalInfo.email || personalInfoDefault.email}
+            </div>
+
+            <div className="px-2 bg-white text-black">Contact:</div>
+
+            <div className="text-white">
+              {personalInfo.phoneNumber || personalInfoDefault.phoneNumber}
+            </div>
+
+            <div className="px-2 bg-white text-black">LinkedIn Url:</div>
+            <div className="text-white">
+              {personalInfo.linkedInUrl || personalInfoDefault.linkedInUrl}
+            </div>
+
+            <div className="px-2 bg-white text-black">Github Url:</div>
+            <div className="text-white">
+              {personalInfo.githubUrl || personalInfoDefault.githubUrl}
+            </div>
+
+            <div className="px-2 bg-white text-black">Address:</div>
+            <div className="text-white">
+              {personalInfo.city || personalInfoDefault.city},{" "}
+              {personalInfo.state || personalInfoDefault.state}
             </div>
           </div>
         </div>
+
+        <div className="flex flex-col p-4 w-8/12 gap-3">
+          <div>
+            <div className="text-justify">{summary || summaryDefault}</div>
+          </div>
+
+          <div>
+            <div className="bg-white px-1 py-0.5 text-lg font-semibold text-[#4b6982] mb-2">
+              Professional Experience
+            </div>
+            <div>
+              {getWorkHistory.map((item) => (
+                <div className="text-sm" key={item.id}>
+                  <div>
+                    <div className="text-lg font-semibold">
+                      {item.positionTitle}
+                    </div>
+                    <div className="flex gap-2">
+                      <span>{item.companyName}</span> |
+                      <span className="opacity-50">
+                        {getMMMYYYYFromDateString(item.startDate)} -
+                        {getMMMYYYYFromDateString(item.endDate)}
+                      </span>
+                    </div>
+                  </div>
+                  <ul>
+                    {item.summary.split(".").map(
+                      (point) =>
+                        point && (
+                          <li className="flex gap-3 my-2" key={point}>
+                            <div className="w-1 h-1 bg-[#DE8534] rounded-full mt-2"></div>
+                            <div>{point}</div>
+                          </li>
+                        )
+                    )}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <div className="bg-light text-lg font-semibold bg-white px-1 py-0.5 text-[#4b6982] mb-2">
+              Education
+            </div>
+            <div>
+              {getEducationData.map((item) => (
+                <div className="text-sm" key={item.id}>
+                  <div className="text-lg font-semibold">{item.degree}</div>
+                  <div className="flex gap-2">
+                    <span>
+                      {item.schoolName}, {item.schoolLocation}
+                    </span>
+                    |
+                    <span className="opacity-50">
+                      {getMMMYYYYFromDateString(item.startDate)} -{" "}
+                      {getMMMYYYYFromDateString(item.endDate)}
+                    </span>
+                  </div>
+                  <ul>
+                    <li className="flex gap-3 my-2">
+                      <div className="w-[6px] h-[6px] bg-[#DE8534] rounded-full mt-2"></div>
+                      <div>{item.description}</div>
+                    </li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <div className="bg-light text-lg font-semibold bg-white px-1 py-0.5 text-[#4b6982]">
+              Key Skills
+            </div>
+            <ul>
+              {getSkillsData.map((item) => (
+                <li className="flex gap-3 my-2" key={item.id}>
+                  <div className="w-[6px] h-[6px] bg-[#DE8534] rounded-full mt-2"></div>
+                  <div>{item.name}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
